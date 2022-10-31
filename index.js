db071576e5724abc9319c459cfc567556e468724
Wilson Mungai <wilsonmungai600@gmail.com>
2022-10-31 17:09:36 -0400

    Added call backs

--- index.js
+++ index.js
@@ -25,3 +25,50 @@ var plugin = function(options)
         console.log('Request Count --> Get: ${getCount} Post: ${postCount}' );
     });
 }
+
+module.exports = plugin;
+
+//call backs
+var seneca = require("seneca")();
+seneca.use (plugin);
+seneca.use('seneca-entity');
+
+seneca.add('role:patient, cmd: add-patient', function(args, done)
+{
+    console.log("********** add-patient");
+
+    var patient =
+    {
+        patientname: args.patientname,
+        patientphone: args.patientphone,
+        patientaddress: args.patientaddress,
+        patientage: args.patientage,
+        patientgender: args.patientgender
+    }
+
+    console.log("--> patient: " + JSON.stringify(patient));
+    seneca.act({ role: 'patient', cmd: 'add', data: patient }, function (err, msg) 
+    {
+        console.log(msg);
+        done(err, msg);
+    });
+
+});
+
+seneca.add('role:patient, cmd:get-patient', function (args, done) {
+    console.log("--> cmd:get-patient, args.patient_id: " + args.patient_id);
+    seneca.act({ role: 'patient', cmd: 'get', data: { patient_id: args.patient_id } }, function (err, msg) 
+    {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+seneca.add('role:patient, cmd:get-all-patients', function (args, done) {
+    console.log("--> cmd:get-all-patients");
+    seneca.act({ role: 'patient', cmd: 'get-all' }, function (err, msg) 
+    {
+        console.log(msg);
+        done(err, msg);
+    });
+});
